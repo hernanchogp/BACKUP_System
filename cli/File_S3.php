703ad712eccc4cb322187c05787de73effeba9a3
Hernan Gonzalez <hernan.gonzalez@entelgy.com>
2022-03-09 12:07:24 -0500

    File transfer s3

---
+++ cli/File_S3.php
@@ -0,0 +1,39 @@
+<?php
+chdir(__DIR__);
+
+require_once '../class/S3_Class.php';
+require_once '../class/Print_Class.php';
+require_once '../config/config.php';
+
+$objPrint = new Print_Class(2, 'Start BACKUP transfer to S3 ...');
+
+/**
+ * Creacion de directorio que almacenara los recursos por dia
+ */
+
+if (!is_dir(PATH_BACKUP) || !file_exists(PATH_BACKUP)) {
+
+    $objPrint->render('The BACKUP directory does not exist or is not valid. ' . PATH_BACKUP, 5);
+    exit;
+}
+
+$pathBK = PATH_BACKUP;
+
+$objPrint->render('Validation finish backup directory. ', 1);
+
+$nameZip = PATH_BACKUP . '/' . 'backup_moodle.tar.gz';
+try {
+    $refBucket = date('Ymd');
+    $objS3 = new S3_class(S3_ACCESSKEY, S3_SECRETKEY, S3_BUCKET);
+    $objS3->cargarArchivoS3($nameZip, $refBucket);
+    $objPrint->render('Finish ZIP file transfer process to S3...', 2);
+    unlink($nameZip);
+} catch (Exception $ex) {
+    $objPrint->render('A novelty was file or S3 transfer ' . $ex->getMessage(), 2);
+    die;
+}
+$objPrint->render('The BACKUP generation process ends successfully for the day... ' . $refBucket, 2);
+
+unset($objPrint);
+unset($objS3);
+//*/
